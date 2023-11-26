70c5340b9a595b1646b5907d4c1c32f317bae4ec
ermix3 <redaermich@gmail.com>
2023-11-26 20:23:06 +0100

    enums

---
+++ logs-logs-logs/src/main/java/LogLine.java
@@ -0,0 +1,25 @@
+public class LogLine {
+    private String logLine;
+
+    public LogLine(String logLine) {
+        this.logLine = logLine;
+    }
+
+    public LogLevel getLogLevel() {
+        String logLevel = this.logLine.substring(1, 4);
+        return switch (logLevel) {
+            case "TRC" -> LogLevel.TRACE;
+            case "DBG" -> LogLevel.DEBUG;
+            case "INF" -> LogLevel.INFO;
+            case "WRN" -> LogLevel.WARNING;
+            case "ERR" -> LogLevel.ERROR;
+            case "FTL" -> LogLevel.FATAL;
+            default -> LogLevel.UNKNOWN;
+        };
+    }
+
+    public String getOutputForShortLog() {
+        String message = this.logLine.substring(this.logLine.indexOf(":") + 2);
+        return getLogLevel().getValue() + ":" + message;
+    }
+}
